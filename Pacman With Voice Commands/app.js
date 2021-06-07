3feebe8c3d5c4a0f8cb3ef49cd8bfb3f47ae97ba
aikozhaoz <aikozhao1203@gmail.com>
2021-06-07 14:34:11 -0400

    Finish up the game. Complete ghost related functions. Add check for win and game over functions!

--- Pacman With Voice Commands/app.js
+++ Pacman With Voice Commands/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let score = 0;
     // Declare a variable called squares which stores all the squares
     const squares = [];
+    // For check if user wins or not
+    let ghostEaten = 0;
+    let powerPelletNum = 0;
 
     // Game Board Architecture:
     // 0: pac dot
@@ -75,11 +78,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // Style pacman based on its location
     squares[pacmanCurrentLocation].classList.add('pac-man');
 
+    document.addEventListener("keyup", movePacman);
+
     // Using switch to move pac-man
     // left = 37
     // up = 38
     // right = 39
     // down = 40
+    // I call all the game logic related functions under this function because all the game logic should happen when pac man is moving
     function movePacman(e){
         squares[pacmanCurrentLocation].classList.remove('pac-man');
         switch(e.keyCode){
@@ -107,13 +113,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 break;
             case 38:
                 // check if pacman is at the top edge now
-                if(((pacmanCurrentLocation-28)>=0)&&(!squares[pacmanCurrentLocation-width].classList.contains('wall'))&&(!squares[pacmanCurrentLocation-width].classList.contains('wall'))){
+                if(((pacmanCurrentLocation-28)>=0)&&(!squares[pacmanCurrentLocation-width].classList.contains('wall'))&&(!squares[pacmanCurrentLocation-width].classList.contains('ghost-lair'))){
                     pacmanCurrentLocation-=width;
                 }
                 break;
             case 40:
                 // check if pacman is at the bottom edge now
-                if(((pacmanCurrentLocation/width)<(width-1))&&(!squares[pacmanCurrentLocation+width].classList.contains('wall'))&&(!squares[pacmanCurrentLocation+width].classList.contains('wall'))){
+                if(((pacmanCurrentLocation/width)<(width-1))&&(!squares[pacmanCurrentLocation+width].classList.contains('wall'))&&(!squares[pacmanCurrentLocation+width].classList.contains('ghost-lair'))){
                     pacmanCurrentLocation+=width;
                 }
                 break;   
@@ -122,9 +128,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
         squares[pacmanCurrentLocation].classList.add('pac-man');
         // Style the game when the pac man eat pac dot
         dotEaten();
+        powerPelletEaten();
+        gameOver();
+        winOrLose();
     }
 
-    document.addEventListener("keyup", movePacman);
+    
 
     // Game Logic
     // Case when Pac dot is eaten by pac man
@@ -133,18 +142,49 @@ document.addEventListener('DOMContentLoaded', ()=>{
         if (squares[pacmanCurrentLocation].classList.contains('pac-dot')){
             score+=dotEatenPoints;
             scoreboard.innerHTML = score;
-            // When the pac dot is eaten, we remove the pac-dot class from the div
+            // When the pac dot is eaten, I remove the pac-dot class from the div
             squares[pacmanCurrentLocation].classList.remove('pac-dot');
         }
     }
     
+    // Case when power pellet is eaten by pac man
+    function powerPelletEaten(){
+        // Check if our pac man ate the power pellet
+        // Check if the current location contains power pellet. If so then our power-pellet is eaten.
+        if(squares[pacmanCurrentLocation].classList.contains("power-pellet")){
+            score += 100;
+            powerPelletNum+=1;
+            // When our pacman ate the power pellet, it can now eat all the ghost so all ghosts now are scared
+            // Change all the ghosts from normal to scared mode
+            ghosts.forEach(ghost => {
+                ghost.isScared = true;
+            });
+            // I do not want all ghosts to be scared for forever, I only want them to be scared for a short period of time.
+            // In this case, I set a timer for about 10 seconds.
+            setTimeout(normalizedGhosts, 10000);
+            squares[pacmanCurrentLocation].classList.remove("power-pellet");
+        }
+    }
+
+    // After power pellet is eaten, all ghosts are scared for a short period of time. In this case
+    // Then all ghost will go back to normal.
+    // Here, I am changing all ghosts back from scared mode to normal.
+    function normalizedGhosts(){
+        ghosts.forEach(ghost => {
+            ghost.isScared = false;
+        })
+    }
+
+
+
     // Create Ghost object
     class Ghost{
-        constructor(className, startIndex, speed){
+        constructor(className, startLocation, speed){
             this.className = className;
-            this.startIndex = startIndex;
+            this.startLocation = startLocation;
             this.speed = speed;
-            this.currentIndex = startIndex;
+            this.currentLocation = startLocation;
+            this.isScared = false;
             this.timerID=NaN;
         }
     }
@@ -160,12 +200,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     // Place the ghosts onto the grid
     ghosts.forEach(ghost => {
-        squares[ghost.currentIndex].classList.add(ghost.className);
-        squares[ghost.currentIndex].classList.add("ghost");
+        squares[ghost.currentLocation].classList.add(ghost.className);
+        squares[ghost.currentLocation].classList.add("ghost");
     });
 
     // Move ghosts randomly
-    ghosts.forEach(ghost => {moveGhost(ghost)})
+    ghosts.forEach(ghost => {
+        moveGhost(ghost);
+    })
     
     // Build move ghosts function
     function moveGhost(ghost){
@@ -179,18 +221,73 @@ document.addEventListener('DOMContentLoaded', ()=>{
         // ID value returned by setInterval() is used as the parameter for the clearInterval() method.
         ghost.timerID = setInterval(function(){
             // If the next square is empty(NO wall and NO any other ghost), then the current ghost can go there
-            // else we have to find a new direction to try
-            if((!squares[ghost.currentIndex+direction].classList.contains("wall"))&&(!squares[ghost.currentIndex+direction].classList.contains("ghost"))){
+            // else I have to find a new direction to try
+            if((!squares[ghost.currentLocation+direction].classList.contains("wall"))&&(!squares[ghost.currentLocation+direction].classList.contains("ghost"))){
                 // If the empty condition is met, then we are going to move our current ghost.
                 // Clear out the current location's class
-                squares[ghost.currentIndex].classList.remove(ghost.className, "ghost","scared-ghost");
+                squares[ghost.currentLocation].classList.remove(ghost.className, "ghost","scared-ghost");
                 // Add its class to its new location
-                ghost.currentIndex+=direction;
-                squares[ghost.currentIndex].classList.add(ghost.className, "ghost");
+                ghost.currentLocation+=direction;
+                squares[ghost.currentLocation].classList.add(ghost.className, "ghost");
             }else{
                 direction = directions[Math.floor(Math.random() * directions.length)];
             }
-            
+            // Check if this ghost is scared now
+            // Ghosts always move. 
+            // When they are scared, they are still moving. So we change ghosts' style(NOT scared => scared) within the setInterval method.
+            if(ghost.isScared){
+                squares[ghost.currentLocation].classList.add("scared-ghost");
+            }
+            // We do not need to change all the ghosts' style from scared to NOT scared because we already set a timer for that.
+            // As soon as 10 seconds hit, normalizeGhosts() will be called and it will set all ghost.isScared = false.
+            // Since the if condition no longer met, all ghosts will automatically change back to normal.
+
+            // When ghosts are scared, they can now be eaten. Because pacman can only eat scared ghosts.
+            // So when the ghosts are scared and pacman runs into it, pacman now eat the scared ghost. 
+            // The ghost will go back to its starting position.
+            if(ghost.isScared && squares[ghost.currentLocation].classList.contains("pac-man")){
+                squares[ghost.currentLocation].classList.remove(ghost.className,"ghost", "scared-ghost");
+                ghost.currentLocation = ghost.startLocation;
+                score += 1000;
+                ghostEaten+=1;
+                squares[currentLocation].classList.add(ghost.className, "ghost");
+            }
+            // Check if game is over now
+            gameOver();
         },ghost.speed);
+    }
+
+    // Check for a game over
+    // Under both movePacman and moveGhost we need to check if the game over condition is met.
+    function gameOver(){
+        // If pac man run into a UNSCARED ghost, pacman dies.
+        // GAME OVER : (
+        if(squares[pacmanCurrentLocation].classList.contains("ghost") && !squares[pacmanCurrentLocation].classList.contains("scared-ghost")){
+            // Game over set up:
+            // Clear time interval for all ghosts
+            ghosts.forEach(ghost => {
+                clearInterval(ghost.timerID);
+            })
+            // Unable the user to move pacman.
+            document.removeEventListener("keyup", movePacman);
+            // Alert user that the game is over now. They loss the game.
+            scoreboard.innerHTML = "GAME OVER!"
+        }
+    }
+   
+    // Check for a win
+    // Because it is only up for pacman's movement on winning so we only call winOrLose() under movePacman()
+    // My rule of winning:
+    // Colleting as many points as you can:
+    // Collect all power pellets
+    // Eat at at least 2 ghosts
+    function winOrLose(){
+        if(powerPelletNum == 4 && ghostEaten ==2){
+            ghosts.forEach(ghost => {
+                clearInterval(ghost.timerID);
+            })
+            document.removeEventListener("keyup", movePacman);
+            scoreboard.innerHTML = "YOU WON!";
+        }
     }
 })
